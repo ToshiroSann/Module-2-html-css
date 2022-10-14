732ad195386d719dce3672ef16c4a6569ed50372
ToshiroSann <owenhomanlaw@gmail.com>
2022-10-14 11:40:37 +0200

    Done

---
+++ week-2-Pump-up-that-CSS-knowledge/Eindopdracht/index.js
@@ -0,0 +1,16 @@
+$('.stack').click(function () {
+    $(".whiteCard").each(function (e) {
+        setTimeout(function () {
+            $(".whiteCard").eq(e).attr("class", "whiteCard");
+        }, e * 150)
+    });
+});
+
+$('.spread').click(function () {
+    $(".whiteCard").each(function (e) {
+        setTimeout(function () {
+            $(".whiteCard").eq(e).attr("class", "whiteCard ani" + e);
+        }, e * 150)
+    });
+});
+
